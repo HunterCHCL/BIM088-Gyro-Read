7080a8135b9999b4ea4682b1b6d2d877e2c0cc16
HunterCHCL <159284508+HunterCHCL@users.noreply.github.com>
2026-05-04 19:26:20 +0800

    修正灵敏度参数，更改数据处理逻辑

--- MDK-ARM/BMI088.c
+++ MDK-ARM/BMI088.c
@@ -1,9 +1,9 @@
 #include "BMI088.h"
-#include <string.h>   /* for memset */
+#include <string.h>
 
 static uint8_t  g_accel_range = BMI088_ACC_RANGE_3G;
 static uint8_t  g_gyro_range  = BMI088_GYRO_RANGE_2000DPS;
-//片选控制函数
+
 static inline void BMI088_ACC_CS_Low(void)
 {
     HAL_GPIO_WritePin(BMI088_CS_ACCEL_PORT, BMI088_CS_ACCEL_PIN, GPIO_PIN_RESET);
@@ -20,7 +20,6 @@ static inline void BMI088_GYRO_CS_High(void)
 {
     HAL_GPIO_WritePin(BMI088_CS_GYRO_PORT, BMI088_CS_GYRO_PIN, GPIO_PIN_SET);
 }
-//延时函数
 static inline void BMI088_Delay_ms(uint32_t ms)
 {
     HAL_Delay(ms);
@@ -95,4 +94,64 @@ void BMI088_Init(void)
     BMI088_Delay_ms(10);
     BMI088_GYRO_Write_Reg(BMI088_GYRO_RANGE, g_gyro_range); //设置陀螺仪量程
     BMI088_Delay_ms(10);
+}
+void BMI088_Read_Accel(BMI088_Data_t *data)
+{
+    uint8_t raw_data[6];
+    BMI088_ACC_Read_Multi_Reg(BMI088_ACC_X_LSB, raw_data, 6);
+    data.ax = BMI088_Accel_Sensitivity() * (float)(int16_t)((raw_data[1] << 8) | raw_data[0]);
+    data.ay = BMI088_Accel_Sensitivity() * (float)(int16_t)((raw_data[3] << 8) | raw_data[2]);
+    data.az = BMI088_Accel_Sensitivity() * (float)(int16_t)((raw_data[5] << 8) | raw_data[4]);
+}
+void BMI088_Read_Gyro(BMI088_Data_t *data)
+{
+    uint8_t raw_data[6];
+    BMI088_GYRO_Read_Multi_Reg(BMI088_GYRO_X_LSB, raw_data, 6);
+    data.gx = BMI088_Gyro_Sensitivity() * (float)(int16_t)((raw_data[1] << 8) | raw_data[0]);
+    data.gy = BMI088_Gyro_Sensitivity() * (float)(int16_t)((raw_data[3] << 8) | raw_data[2]);
+    data.gz = BMI088_Gyro_Sensitivity() * (float)(int16_t)((raw_data[5] << 8) | raw_data[4]);
+}
+void BMI088_Set_Accel_Range(uint8_t range)
+{
+    g_accel_range = range;
+    BMI088_ACC_Write_Reg(BMI088_ACC_RANGE, g_accel_range);
+}
+void BMI088_Set_Gyro_Range(uint8_t range)
+{
+    g_gyro_range = range;
+    BMI088_GYRO_Write_Reg(BMI088_GYRO_RANGE, g_gyro_range);
+}
+static float BMI088_Accel_Sensitivity()
+{
+    switch (g_accel_range)
+    {
+        case BMI088_ACC_RANGE_3G:
+            return BMI088_ACC_SENS_3G;
+        case BMI088_ACC_RANGE_6G:
+            return BMI088_ACC_SENS_6G;
+        case BMI088_ACC_RANGE_12G:
+            return BMI088_ACC_SENS_12G;
+        case BMI088_ACC_RANGE_24G:
+            return BMI088_ACC_SENS_24G;
+        default:
+            return BMI088_ACC_SENS_3G;
+    }
+}
+static float BMI088_Gyro_Sensitivity()
+{
+    switch (g_gyro_range)
+    {
+        case BMI088_GYRO_RANGE_2000DPS:
+            return BMI088_GYRO_SENS_2000DPS;
+        case BMI088_GYRO_RANGE_1000DPS:
+            return BMI088_GYRO_SENS_1000DPS;
+        case BMI088_GYRO_RANGE_500DPS:
+            return BMI088_GYRO_SENS_500DPS;
+        case BMI088_GYRO_RANGE_250DPS:
+            return BMI088_GYRO_SENS_250DPS;
+        case BMI088_GYRO_RANGE_125DPS:
+            return BMI088_GYRO_SENS_125DPS;
+        default:
+            return BMI088_GYRO_SENS_2000DPS;
+    }
 }
--- MDK-ARM/BMI088.h
+++ MDK-ARM/BMI088.h
@@ -39,15 +39,26 @@
 #define BMI088_GYRO_RANGE_250DPS 0x03
 #define BMI088_GYRO_RANGE_125DPS 0x04
 
-#define BMI088_ACC_RANGE_3G_SENS 0.00059814453125f
-#define BMI088_ACC_RANGE_6G_SENS 0.0011962890625f
-#define BMI088_ACC_RANGE_12G_SENS 0.002392578125f
-#define BMI088_ACC_RANGE_24G_SENS 0.00478515625f
-#define BMI088_GYRO_RANGE_2000DPS_SENS 0.06103515625f
-#define BMI088_GYRO_RANGE_1000DPS_SENS 0.030517578125f
-#define BMI088_GYRO_RANGE_500DPS_SENS 0.0152587890625f
-#define BMI088_GYRO_RANGE_250DPS_SENS 0.00762939453125f
-#define BMI088_GYRO_RANGE_125DPS_SENS 0.003814697265625f
+#define BMI088_ACC_SENS_3G 0.0008979988098144531f
+#define BMI088_ACC_SENS_6G 0.0017959976196289062f
+#define BMI088_ACC_SENS_12G 0.0035919952392578125f
+#define BMI088_ACC_SENS_24G 0.007183990478515625f
+#define BMI088_GYRO_SENS_2000DPS 0.06103515625f
+#define BMI088_GYRO_SENS_1000DPS 0.030517578125f
+#define BMI088_GYRO_SENS_500DPS 0.0152587890625f
+#define BMI088_GYRO_SENS_250DPS 0.00762939453125f
+#define BMI088_GYRO_SENS_125DPS 0.003814697265625f
+
+typedef struct {
+    float ax;
+    float ay;
+    float az;
+    float gx;
+    float gy;
+    float gz;
+} BMI088_Data_t;
+
+extern BMI088_Data_t BMI088_Data;
 
 void BMI088_ACC_Write_Reg(uint8_t addr, uint8_t data);
 void BMI088_ACC_Write_Multi_Reg(uint8_t addr, const uint8_t *data, uint16_t len);
@@ -58,5 +69,11 @@ void BMI088_GYRO_Write_Reg(uint8_t addr, uint8_t data);
 void BMI088_GYRO_Write_Multi_Reg(uint8_t addr, const uint8_t *data, uint16_t len);
 void BMI088_GYRO_Read_Reg(uint8_t addr, uint8_t *data);
 void BMI088_GYRO_Read_Multi_Reg(uint8_t addr, uint8_t *data, uint16_t len);
+
+void BMI088_Init(void);
+void BMI088_Read_Accel(BMI088_Data_t *data);
+void BMI088_Read_Gyro(BMI088_Data_t *data);
+void BMI088_Set_Accel_Range(uint8_t range);
+void BMI088_Set_Gyro_Range(uint8_t range);
 
 #endif /* __BMI088_H__ */
