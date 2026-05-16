1e52978be2d55967088bf8e37b87ab19646354b4
HunterCHCL <159284508+HunterCHCL@users.noreply.github.com>
2026-05-17 00:57:25 +0800

    修复bug

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -116,15 +116,12 @@ int main(void)
   {
 		uint32_t now = HAL_GetTick();
 		float dt = (now - last_tick) / 1000.0f;
-		last_tick = now;
-
 		BMI088_Update_Angle(dt);
+		last_tick = now;
     if(i++>=10)
     {
 		  sprintf(buf, "Angle: %.2f %.2f %.2f  Accel: %.2f %.2f %.2f Gyro: %.2f %.2f %.2f\r\n",
-				BMI088_Data.angle.roll, BMI088_Data.angle.pitch, BMI088_Data.angle.yaw,
-				BMI088_Data.ax, BMI088_Data.ay, BMI088_Data.az,
-				BMI088_Data.gx, BMI088_Data.gy, BMI088_Data.gz);
+			BMI088_Data.angle.roll, BMI088_Data.angle.pitch, BMI088_Data.angle.yaw,BMI088_Data.ax, BMI088_Data.ay, BMI088_Data.az,BMI088_Data.gx, BMI088_Data.gy, BMI088_Data.gz);
 		  CDC_Transmit_FS((uint8_t*)buf, strlen(buf));
       i = 0;
     }
